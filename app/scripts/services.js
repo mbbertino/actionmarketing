a5af285279c93ddd326bf7bf9775dd39ab2ddb53
Matt Bertino <mattbertino@gmail.com>
2014-06-19 11:11:47 -0400

    added in a side nav bar to the service page and finalized the format of this page

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -7,6 +7,19 @@ var ServicesView = Backbone.View.extend({
   initialize: function() {
     $('.content-wrapper').html(this.el)
     this.render()
+
+    doThisStuffOnScroll = function() {
+      var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+      if (scrollTop > 140) {
+        $('.docs-sidebar').removeClass('affix-top')
+        $('.docs-sidebar').addClass('affix')
+      } else {
+        $('.docs-sidebar').removeClass('affix')
+        $('.docs-sidebar').addClass('affix-top')
+      }
+    }
+
+    window.onscroll = doThisStuffOnScroll;
   },
 
   render: function() {
