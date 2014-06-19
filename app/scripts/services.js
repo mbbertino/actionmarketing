0c7084721bc174245ca2203cb43a364e2bcf917d
Matt Bertino <mattbertino@gmail.com>
2014-06-19 11:24:42 -0400

    added in the auto scrolling to the side navbar on the services page

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -2,7 +2,11 @@ var ServicesView = Backbone.View.extend({
 
   renderedTemplate: _.template($('#services-template').text()),
 
-  events: {},
+  events: {
+    "click .js-nav-one": "moveToOne",
+    "click .js-nav-two": "moveToTwo",
+    "click .js-nav-three": "moveToThree",
+  },
 
   initialize: function() {
     $('.content-wrapper').html(this.el)
@@ -20,6 +24,24 @@ var ServicesView = Backbone.View.extend({
     }
 
     window.onscroll = doThisStuffOnScroll;
+  },
+
+  moveToOne: function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1000);
+  },
+
+  moveToTwo: function() {
+    $('html, body').animate({
+      scrollTop: $('#fill').offset().top - 15
+    }, 1000);
+  },
+
+  moveToThree: function() {
+    $('html, body').animate({
+      scrollTop: $('#data').offset().top - 15
+    }, 1000);
   },
 
   render: function() {
