1ddcf25d216bb3feed34a3ec5697dac675cce735
Matt Bertino <mattbertino@gmail.com>
2014-07-06 18:33:48 -0400

    added about page w/ goog map and marker and request quote section

--- app/scripts/views/about.js
+++ app/scripts/views/about.js
@@ -1,5 +1,7 @@
 var AboutView = Backbone.View.extend({
 
+  className: "about-page",
+
   renderedTemplate: _.template($('#about-template').text()),
 
   events: {
@@ -9,6 +11,27 @@ var AboutView = Backbone.View.extend({
   initialize: function() {
     $('.content-wrapper').html(this.el)
     this.render()
+
+    var map;
+
+    function initialize() {
+      var myLatlng = new google.maps.LatLng(26.924099, -80.144593);
+      var mapOptions = {
+        zoom: 14,
+        center: myLatlng
+      };
+      map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+
+      var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: 'Action Marketing'
+      });
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+
   },
 
   render: function() {
